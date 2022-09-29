ec1e170019bd9676838f9bbabc43db9ea6393e15
Bilal <89836993+Bilal2903@users.noreply.github.com>
2022-09-29 13:12:15 +0200

    Vandaag 29 september inlog en registratie done

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -15,6 +15,7 @@
                     @endif
 
                     {{ __('You are logged in!') }}
+                    <a href="/about">Naar About</a>
                 </div>
             </div>
         </div>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AboutController;
+use App\Http\Controllers\HomeController;
 use App\Http\Controllers\MotorController;
 use Illuminate\Support\Facades\Route;
 
@@ -15,9 +16,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('home');
-});
+Route::get('/', [HomeController::class, 'index']);
+
 
 Route::get('/about', [AboutController::class, 'show']);
 
@@ -25,4 +25,4 @@ Route::get('/motorPage', [MotorController::class, 'show']);
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/home', [HomeController::class, 'index'])->name('home');
