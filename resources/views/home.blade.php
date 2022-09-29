bc6a864cae8cf7dc4ababe8d951eba169110bd6c
Bilal <89836993+Bilal2903@users.noreply.github.com>
2022-09-29 12:41:06 +0200

    Vandaag 29 september de database af gemaakt. De database laat ik zien op de motor page waar het hoort.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,6 +3,6 @@
 @section('content')
 
     <h1>Home</h1>
-    <a href="/motorPage"> Motor Page</a>
-
+    <a href="/motorPage"> Motor Page</a> <br>
+    <a href="/about"> About Page</a> <br>
 @endsection
--- resources/views/motorPage.blade.php
+++ resources/views/motorPage.blade.php
@@ -3,6 +3,10 @@
 @section('content')
 
     <h1>Motor Page</h1>
-    <a href="/about"> About</a>
+    <a href="/about"> About</a> <br>
+    <a href="/"> Home Page</a> <br>
+
+
+    <p> {{$motor}}</p>
 
 @endsection
